83cf3e6d9732c02d8175a32c23709b7c9257d72f
Anders Boyum <Acboyum@gmail.com>
2023-04-27 14:29:40 -0500

    fixed router and ListItem syntax

--- server/routers/groceries.router.js
+++ server/routers/groceries.router.js
@@ -3,5 +3,16 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 
+// GET route for groceries list
+router.get('/', (req, res) => {
+    console.log("In GET req");
+
+    let sqlText = `
+        SELECT * FROM "groceries"
+            WHERE 
+                ORDER BY "purchased" ASC,
+                "name" ASC;
+    `
+})
 
 module.exports = router;
--- src/components/ListItems/ListItems.jsx
+++ src/components/ListItems/ListItems.jsx
@@ -20,7 +20,7 @@ function ListItems(props) {
     }
 
     return(
-
+        
 
     )
 }
