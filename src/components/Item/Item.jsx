d97e767657949756a28da72e5669f1f670d9b14f
mandi andrade <mandi.andrade20@gmail.com>
2023-04-27 15:50:40 -0500

    initial attempt at Item function

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,9 +1,21 @@
+
 // Targeting the Grocery Items with a new component:
 
 function Item(props) {
     return (
         <ul>
-            
+            {
+            props.groceries.map((grocery) => {
+                return (
+                    <li key={props.grocery.id}>
+                        <p>{props.grocery.name}</p>
+                        <p>{props.grocery.quantity} {props.grocery.unit}</p>  
+                        <button>Buy</button>
+                        <button>Remove</button>
+                    </li>
+                )
+            })
+        }
         </ul>
     )
 }
