209402d5147ed189b876e14200d16b9c240c6c22
Kobi Jones <kobijonesmail@gmail.com>
2023-04-27 13:50:02 -0500

    Add database SQL setup

--- database.sql
+++ database.sql
@@ -1,2 +1,18 @@
--- Don't forget to add your create table SQL 
--- It is also helpful to include some test data
+-- Create table
+CREATE TABLE "groceries" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"quantity" REAL NOT NULL,
+	"unit" VARCHAR(20) DEFAULT NULL,
+	"purchased" BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- Sample data
+INSERT INTO "groceries"
+	("name", "quantity", "unit", "purchased")
+VALUES
+	('Apples', 1, NULL, FALSE),
+	('Flour', 2.1, 'meters', TRUE),
+	('Beans', 1, 'pounds', FALSE),
+	('Zucchini', 4, NULL, TRUE),
+	('Beef', 0.3, 'seconds', FALSE);
