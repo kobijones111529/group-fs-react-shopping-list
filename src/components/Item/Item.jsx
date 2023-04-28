d066661c29a86a8c85dbb83cdb33716f81fd8819
Anders Boyum <Acboyum@gmail.com>
2023-04-28 09:52:06 -0500

    Began to add buy button funcitonality. Added conditional (if 'purchased' === false) to PUT req

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,33 @@
-
+import axios from 'axios';
+import { useState } from 'react';
 // Targeting the Grocery Items with a new component:
 
 function Item(props) {
+    const [isBought, setIsBought] = useState('false')
+    // need a click handler for the buy button to conditionally render 
+    const handleBuy = () => {
+        // check if grocery.puchased is FALSE
+        if(!props.purchased){
+            console.log('TRIED TO BUY')
+            axios({
+                method: 'PUT',
+                url: `/groceries/buy/${props.grocery.id}`,
+                data: 'true'
+            }).then ((res) => {
+                // need to run our conditional render function here
+                console.log("Congratulations on your purchase 🍻");
+            }).catch((err) => {
+                console.log("Couldn't process purchase", err);
+            })
+        }
+    }
+
+
     return (
         <>
             <p>{props.name}</p>
             <p>{props.quantity} {props.unit}</p>
-            <button>Buy</button>
+            <button onClick={handleBuy}>Buy</button>
             <button>Remove</button>
         </>
     );
