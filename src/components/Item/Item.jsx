4b88034842ed6c77ab30539f34584d712a9d66e0
Anders Boyum <Acboyum@gmail.com>
2023-04-28 10:20:35 -0500

    got client-side buy button functionality working, need to make a PUT req receiver on server-side

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,22 +3,38 @@ import { useState } from 'react';
 // Targeting the Grocery Items with a new component:
 
 function Item(props) {
-    const [isBought, setIsBought] = useState('false')
+    // const [isBought, setIsBought] = useState('false')
+
     // need a click handler for the buy button to conditionally render 
     const handleBuy = () => {
-        // check if grocery.puchased is FALSE
-        if(!props.purchased){
-            console.log('TRIED TO BUY')
-            axios({
-                method: 'PUT',
-                url: `/groceries/buy/${props.grocery.id}`,
-                data: 'true'
-            }).then ((res) => {
-                // need to run our conditional render function here
-                console.log("Congratulations on your purchase 🍻");
-            }).catch((err) => {
-                console.log("Couldn't process purchase", err);
-            })
+
+        console.log('TRIED TO BUY')
+        axios({
+            method: 'PUT',
+            url: `/groceries/buy/${props.id}`,
+            data: 'true'
+        }).then ((res) => {
+            console.log("Congratulations on your purchase 🍻");
+        }).catch((err) => {
+            console.log("Couldn't process purchase", err);
+        })
+    
+    }
+
+    // conditional render of buttons if an item is purchased
+    const renderButtons = () => {
+        if (!props.purchased) {
+            return (
+                <p>Purchased</p>
+            )
+        }
+        else {
+            return (
+                <>
+                    <button onClick={handleBuy}>Buy</button>
+                    <button>Remove</button>
+                </>
+            )
         }
     }
 
@@ -27,8 +43,7 @@ function Item(props) {
         <>
             <p>{props.name}</p>
             <p>{props.quantity} {props.unit}</p>
-            <button onClick={handleBuy}>Buy</button>
-            <button>Remove</button>
+            <p>{renderButtons()}</p>
         </>
     );
 }
