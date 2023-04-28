773d2497c3c77e76a557aea0dba382e131c0d354
Kobi Jones <kobijonesmail@gmail.com>
2023-04-28 09:46:19 -0500

    Hoist get request and grocery list state from ListItems to App

--- src/components/ListItems/ListItems.jsx
+++ src/components/ListItems/ListItems.jsx
@@ -3,30 +3,9 @@ import axios from 'axios';
 import Item from '../Item/Item';
 
 function ListItems(props) {
-    const [groceryList, setGroceryList] = useState([]);
-
-    // is it necessary to have this in useEffect? I believe so
-    useEffect(() => {
-        fetchGroceries();
-    }, [])
-
-    // get the groceries array
-    const fetchGroceries = () => {
-        axios({
-            method: 'GET',
-            url: '/groceries',
-        }).then((res) => {
-            setGroceryList(res.data);
-            console.log("Got our grocery list", groceryList);
-        }).catch((err) => {
-            console.log("Couldn't get grocery list", err)
-        })
-    }
-
-
     return(
         <ul>
-            {groceryList.map(grocery => {
+            {props.groceryList.map(grocery => {
                 return (
                     <li key={grocery.id}>
                         <Item
